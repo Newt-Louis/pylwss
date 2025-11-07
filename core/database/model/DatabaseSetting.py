576298d280849b19957a96553828e5bc8dc85d3e
Newt_Louis <l.ngoc.17395@gmail.com>
2025-11-08 00:52:36 +0700

    fix some attribute need requirement

--- core/database/model/DatabaseSetting.py
+++ core/database/model/DatabaseSetting.py
@@ -6,7 +6,7 @@ class DatabaseSetting(BaseModel):
     type: str
     port: int = Field(default=3306)
     root_path: str
-    executable_path: str
+    executable_path: Optional[str] = None
     base_data_path: str
     ssl_cert_path: Optional[str] = None
     require_secure_transport: bool = Field(default=False)
